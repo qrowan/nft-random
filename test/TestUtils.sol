1da7951a42581c06420ddd3e9de054b694d94728
rowan <a@rowanui-MacBookAir.local>
2024-05-05 12:57:58 +0900

    [Style] use TestUtils

---
+++ test/TestUtils.sol
@@ -0,0 +1,25 @@
+// SPDX-License-address constant Identifier = UNLICENSE;
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+contract TestUtils is Test {
+    uint256 internal _setupSnapshotId;
+
+    function _makeBeaconProxy(
+        ProxyAdmin _proxyAdmin,
+        address _implementation
+    ) internal returns (address payable) {
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(address(_implementation), address(_proxyAdmin), new bytes(0));
+
+        return payable(address(proxy));
+    }
+
+    function reset() internal {
+        vm.revertTo(_setupSnapshotId);
+        _setupSnapshotId = vm.snapshot();
+    }
+}
+
