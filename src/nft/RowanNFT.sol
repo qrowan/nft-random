50ff47b26641a3baaac04ec1cc2fe4ee350c7cad
rowan <hsjii1589@gmail.com>
2024-05-05 21:38:59 +0900

    [Feat] random by VRF (WIP)

--- src/nft/RowanNFT.sol
+++ src/nft/RowanNFT.sol
@@ -1,20 +1,34 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
+import "../chainlink_libraries/VRFConsumerBaseV2Upgradeable.sol";
+import "../libraries/Constant.sol";
+
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
-import "../chainlink_libraries/VRFConsumerBaseV2Upgradeable.sol";
+
 import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
 
 contract RowanNFT is ERC721Upgradeable, Ownable2StepUpgradeable, VRFConsumerBaseV2Upgradeable {
     using StringsUpgradeable for uint256;
 
+    uint public price; // price for 1 NFT
     string public defaultURI;
     string public baseURI;
-    uint public price; // price for 1 NFT
-    bool public revealed = false;
-    uint public tokenLength = 0;
-    VRFCoordinatorV2Interface public constant COORDINATOR = VRFCoordinatorV2Interface(0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625);
+    bool public revealed;
+
+    uint public tokenLength;
+    uint public constant MAX_SUPPLY = 20;
+
+    VRFCoordinatorV2Interface public COORDINATOR;
+    LinkTokenInterface public LINK;
+
+    uint64 public s_subscriptionId;
+    uint requestId;
+    uint public requestStatus = 0; // 0: not requested, 1: requested, 2: fulfilled
+    uint256[] randomWords;
+
 
     error NonexistentId(uint tokenId);
 
@@ -24,9 +38,13 @@ contract RowanNFT is ERC721Upgradeable, Ownable2StepUpgradeable, VRFConsumerBase
         __VRFConsumerBaseV2_init(address(COORDINATOR));
 
         // TODO : fix URI
-        baseURI = "https://storage.googleapis.com/opensea-prod.appspot.com/puffs/";
-        defaultURI = "https://openseacreatures.io/";
+        baseURI = Constant.BASE_URI;
+        defaultURI = Constant.DEFAULT_URI;
         price = 0.00001 ether;
+        COORDINATOR = VRFCoordinatorV2Interface(Constant.VRF_COORDINATOR);
+        LinkTokenInterface(Constant.LINK);
+
+        s_subscriptionId = COORDINATOR.createSubscription();
     }
 
     function _baseURI() internal view virtual override returns (string memory) {
@@ -52,13 +70,16 @@ contract RowanNFT is ERC721Upgradeable, Ownable2StepUpgradeable, VRFConsumerBase
     }
 
     function convert(uint tokenId) internal returns (uint) {
-        return tokenId;
+        if (!revealed) return 0;
+        return randomWords[tokenId] % 25;
     }
 
     // purchase NFT
     function mint(uint _mintAmount) public payable {
         uint pay = price * _mintAmount;
         require(msg.value >= pay, "Not enough");
+        require(tokenLength < MAX_SUPPLY, "Cannot mint");
+
         for (uint i; i < _mintAmount; i++) {
             _safeMint(msg.sender, tokenLength);
             tokenLength++;
@@ -68,7 +89,39 @@ contract RowanNFT is ERC721Upgradeable, Ownable2StepUpgradeable, VRFConsumerBase
         }
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+    // Assumes the subscription is funded sufficiently.
+    function requestRandomWords()
+    external
+    onlyOwner
+    {
+        require(requestStatus == 0, "Already requested");
 
+        // Will revert if subscription is not set and funded.
+        uint _requestId = COORDINATOR.requestRandomWords(
+            Constant.KEY_HASH,
+            s_subscriptionId,
+            Constant.REQUEST_CONFIRMATIONS,
+            Constant.CALL_BACK_GAS_LIMIT,
+            uint32(MAX_SUPPLY)
+        );
+        requestStatus = 1;
+        requestId = _requestId;
+
+        // TODO : delete after finishing VRF
+        {
+            uint[] memory _randomWords = new uint[](25);
+            for (uint i; i < 25; i++) {
+                _randomWords[i] = i;
+            }
+            randomWords = _randomWords;
+        }
+    }
+
+    function fulfillRandomWords(
+        uint256 /*_requestId*/, uint256[] memory _randomWords
+    ) internal override {
+        require(requestStatus == 1, "Not requested or already fulfilled");
+        randomWords = _randomWords;
+        requestStatus = 2;
     }
 }
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -7,6 +7,9 @@ import {TestUtils} from "./TestUtils.sol";
 import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {StringsUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/StringsUpgradeable.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "src/libraries/Constant.sol";
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
 
 contract TestContract is Test, TestUtils {
     using StringsUpgradeable for uint256;
@@ -17,15 +20,18 @@ contract TestContract is Test, TestUtils {
     ProxyAdmin proxyAdmin;
     uint[] userTokens;
     error NonexistentId(uint);
+    IERC20Metadata LINK = IERC20Metadata(Constant.LINK);
 
 
     function setUp() public {
         deployer = address(0x1234);
         user = address(0x1235);
+        deal(address(LINK), deployer, 100 ether);
 
         vm.startPrank(deployer);
         {
             proxyAdmin = new ProxyAdmin();
+            LINK.transfer(address(proxyAdmin), 100 ether);
             RowanNFT _rowanNFT = new RowanNFT();
             rowanNFT = RowanNFT(_makeBeaconProxy(proxyAdmin, address(_rowanNFT)));
             rowanNFT.initialize();
@@ -64,20 +70,37 @@ contract TestContract is Test, TestUtils {
     }
 
     function testMintFail() public {
-        vm.deal(user, rowanNFT.price() -1);
+        vm.deal(user, rowanNFT.price() - 1);
         vm.prank(user);
         vm.expectRevert("Not enough");
-        rowanNFT.mint(1);
+        rowanNFT.mint{value: user.balance}(1);
     }
+
 
     function testMintSuccess() public {
         uint mintAmount = 10;
         vm.deal(user, rowanNFT.price() * mintAmount + 1);
         vm.prank(user);
-        rowanNFT.mint{value: user.balance}(mintAmount);
-        assertEq(user.balance, 1, "wrong user balance");
+        rowanNFT.mint{value: user.balance}(mintAmount); // transferred all balance
+        assertEq(user.balance, 1, "wrong user balance"); // refunded 1 as change
         assertEq(address(rowanNFT).balance, rowanNFT.price() * mintAmount, "wrong contract balance");
         assertEq(rowanNFT.balanceOf(user), mintAmount, "wrong mintAmount");
+    }
+
+//    function testMintMaxFail() public {
+//        vm.deal(user, rowanNFT.price() * rowanNFT.MAX_SUPPLY() + 1);
+//        vm.prank(user);
+//        rowanNFT.mint{value: user.balance}(rowanNFT.MAX_SUPPLY());
+//    }
+
+    function testMintMax() public {
+        vm.deal(user, rowanNFT.price() * (rowanNFT.MAX_SUPPLY() + 1));
+        vm.startPrank(user);
+        rowanNFT.mint{value: user.balance}(rowanNFT.MAX_SUPPLY());
+        vm.expectRevert("Cannot mint");
+        rowanNFT.mint{value: user.balance}(1);
+        vm.stopPrank();
+
     }
 
     function testBaseAndDefaultURI() public {
@@ -100,13 +123,15 @@ contract TestContract is Test, TestUtils {
 
     function testUserTokenURIBeforeRevealed() public {
         userMint();
-        uint tokenId = userTokens[0];
-        assertEq(
-            rowanNFT.tokenURI(tokenId),
-            string(abi.encodePacked("https://openseacreatures.io/", tokenId.toString())),
-            "wrong unrevealed URI"
-        );
-        console.log("unrevealed URI : ", rowanNFT.tokenURI(tokenId));
+        for (uint i; i < userTokens.length; i++) {
+            uint tokenId = userTokens[i];
+            assertEq(
+                rowanNFT.tokenURI(tokenId),
+                string(abi.encodePacked("https://openseacreatures.io/", tokenId.toString())),
+                "wrong unrevealed URI"
+            );
+            console.log("unrevealed URI : ", rowanNFT.tokenURI(tokenId));
+        }
     }
 
     function testNonexistentIdURIAfterRevealedFail() public {
@@ -118,13 +143,23 @@ contract TestContract is Test, TestUtils {
     function testUserTokenURIAfterRevealed() public {
         userMint();
         reveal();
-        uint tokenId = userTokens[0];
-        console.log("revealed URI : ", rowanNFT.tokenURI(tokenId));
-        assertEq(
-            rowanNFT.tokenURI(tokenId),
-            string(abi.encodePacked("https://storage.googleapis.com/opensea-prod.appspot.com/puffs/", tokenId.toString(), ".png")),
-            "wrong revealed URI"
-        );
+        for (uint i; i < userTokens.length; i++) {
+            uint tokenId = userTokens[i];
+            console.log("revealed URI : ", rowanNFT.tokenURI(tokenId));
+            assertEq(
+                rowanNFT.tokenURI(tokenId),
+                string(abi.encodePacked("https://storage.googleapis.com/opensea-prod.appspot.com/puffs/", tokenId.toString(), ".png")),
+                "wrong revealed URI"
+            );
+        }
+    }
+
+    function testLogSubscriptionId() public {
+        console.log("Subscription ID ", rowanNFT.s_subscriptionId());
+    }
+
+    function testVRF() public {
+        // TODO : test about VRF after deploy
     }
 
 }
